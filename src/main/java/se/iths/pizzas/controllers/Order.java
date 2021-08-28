8d65c78b87324c6f0351d450992e52ce3cc3f3c6
JonathanLissman <facehump90@gmail.com>
2021-08-28 20:04:37 +0200

    added test to postrequest

--- src/main/java/se/iths/pizzas/controllers/Order.java
+++
@@ -1,6 +0,0 @@
-package se.iths.pizzas.controllers;
-
-public class Order {
-    String text;
-
-}
--- src/test/java/se/iths/pizzas/controllers/PizzaMVCTest.java
+++ src/test/java/se/iths/pizzas/controllers/PizzaMVCTest.java
@@ -1,14 +1,21 @@
 package se.iths.pizzas.controllers;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import se.iths.pizzas.entities.Pizza;
 import se.iths.pizzas.repositories.PizzaRepository;
+import springfox.documentation.spring.web.json.Json;
 
+import static org.springframework.http.RequestEntity.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
@@ -26,4 +33,19 @@ class PizzaMVCTest {
         mockMvc.perform(MockMvcRequestBuilders.get("/pizzas"))
                 .andExpect(status().is(200));
     }
+
+    @Test
+    void addAPizza() throws Exception{
+        mockMvc.perform(
+                MockMvcRequestBuilders.post("/pizzas")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("""
+                                {
+                                "name":"Göran Special",
+                                "price":"20",
+                                "ingredients":"Göran, Ost, Göran, Ost"
+                                }"""))
+                .andExpect(status().isOk());
+
 }
+    }
